750a56d028734b02d50aa0d4d76e804d98a93e9b
Sol <solzito.1889@gmail.com>
2023-09-07 22:48:44 -0300

    se cambia extension

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import RickyReducer from "./todo/rickySlice";
+
+const store = configureStore({
+  reducer: {
+    RickyReducer: RickyReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+type DispatchFunc = () => AppDispatch;
+
+export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
