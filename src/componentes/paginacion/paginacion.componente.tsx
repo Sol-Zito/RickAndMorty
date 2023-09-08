a46f24063d06d17c2f215d00a4308f5e2f7cea71
Sol <solzito.1889@gmail.com>
2023-09-07 22:50:11 -0300

    se cambia extencion y se agrega datos a tomar como props

---
+++ src/componentes/paginacion/paginacion.componente.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./paginacion.css";
+import { fetchCharacter } from "../../store/todo/rickySlice";
+import { useAppDispatch, useAppSelector } from "../../store";
+
+/**
+ * Componente que contiene los botones para paginar
+ *
+ * Deberás agregar las propiedades necesarias para que funcione correctamente
+ *
+ *
+ * @returns un JSX element
+ */
+
+type paginacion = {
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Paginacion = ({ page, setPage }: paginacion) => {
+  return (
+    <div className="paginacion">
+      <button
+        disabled={page === 1 ? true : false}
+        className={"primary"}
+        onClick={() => setPage(page - 1)}
+      >
+        Anterior
+      </button>
+      <button
+        disabled={false}
+        className={"primary"}
+        onClick={() => setPage(page + 1)}
+      >
+        Siguiente
+      </button>
+    </div>
+  );
+};
+
+export default Paginacion;
