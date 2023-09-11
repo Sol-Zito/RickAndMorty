129c12ffbacbccf2cc173953cd28f69b44358c36
Sol <solzito.1889@gmail.com>
2023-09-11 20:03:22 -0300

    documentacion

--- src/componentes/botones/boton-favorito.componente.tsx
+++ src/componentes/botones/boton-favorito.componente.tsx
@@ -1,38 +1,35 @@
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../store";
-import { AGREGAR_FAVORITO } from "../../store/todo/rickySlice";
+import { useAppDispatch } from "../../store";
+import { TOGGLE_FAVORITE, DatosPersonaje } from "../../store/todo/rickySlice";
 import "./boton-favorito.css";
 /**
  * Boton que indica si un elemento es favorito o no, y da la posibilidad de marcarlo/desmarcarlo
  *
  * Deberás tipar las propiedades si usas este componente
+ * @param {boolean} esFavorito variable donde determina si el personaje esta en el array de favoritos
+ * @param {DatosPersonaje} idCharacter informacion del personaje
+ * @param {funcion} setCharacterFav funcion donde se cambia el valor de la variable esFavorito
  *
  *
- * @returns un JSX element
+ * @returns un TSX element
  */
 
 type Datos = {
   esFavorito: boolean;
-  idPersonaje?: number;
-  setAgregadoAFav: React.Dispatch<React.SetStateAction<boolean>>;
+  idCharacter?: DatosPersonaje;
+  setCharacterFav: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const BotonFavorito = ({ esFavorito, idPersonaje, setAgregadoAFav }: Datos) => {
+const BotonFavorito = ({ esFavorito, idCharacter, setCharacterFav }: Datos) => {
   const src = esFavorito ? "/imagenes/star-filled.png" : "/imagenes/star.png";
-
-  const personaje = useAppSelector(
-    (state) => state.RickyReducer.personajes.personajes
-  );
-  const personajeRecibido = personaje.find((item) => item.id === idPersonaje);
-
   const dispatch = useAppDispatch();
-  const addToFav = () => {
-    dispatch(AGREGAR_FAVORITO(personajeRecibido));
-    setAgregadoAFav(!esFavorito);
-  };
-
   return (
-    <div className="boton-favorito" onClick={addToFav}>
+    <div
+      className="boton-favorito"
+      onClick={() => {
+        dispatch(TOGGLE_FAVORITE(idCharacter));
+        setCharacterFav(!esFavorito);
+      }}
+    >
       <img src={src} alt={"favorito"} />
     </div>
   );
