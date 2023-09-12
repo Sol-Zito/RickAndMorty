24a4a5e00d8f0fa5886bf64a8865cfae531f4ce9
Sol <solzito.1889@gmail.com>
2023-09-11 21:19:36 -0300

    se agrega validacion para btn favorito

--- src/paginas/Detalle.pagina.tsx
+++ src/paginas/Detalle.pagina.tsx
@@ -1,38 +1,50 @@
 import "./Detalle.css";
-import BotonFavorito from "../componentes/botones/boton-favorito.componente";
 import TarjetaEpisodio from "../componentes/episodios/tarjeta-episodio.componente";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../store";
 import { EpisodeDates, getEpisodes } from "../store/todo/rickySlice";
 import { useParams } from "react-router-dom";
+import BotonFavorito from "../componentes/botones/boton-favorito.componente";
 
 /**
  * Esta es la pagina de detalle. Aqui se puede mostrar la vista sobre el personaje seleccionado
  *  junto con la lista de episodios en los que aparece
  *
  * EL TRABAJO SOBRE ESTE ARCHIVO ES OPCIONAL Y NO ES REQUISITO DE APROBACION
- *
- *
- *
  * Uso:
  * ``` <PaginaDetalle /> ```
  *
  * @returns la pagina de detalle
  */
+
 const PaginaDetalle = () => {
   const { id } = useParams();
   const episodesDispatch = useAppDispatch();
+
   const [functionExecuted, setFunctionExecuted] = useState(false);
+  const favorites = useAppSelector((state) => state.RickyReducer.favorites);
   const allCharacters = useAppSelector(
     (state) => state.RickyReducer.characters.characters
   );
-  const characterToUse = allCharacters.find((item) => item.id === Number(id));
+  const characterToUse =
+    allCharacters.find((item) => item.id === Number(id)) ??
+    favorites.find((item) => item.id === Number(id));
+  const isFav = favorites.find((item) => item.id === Number(id)) ? true : false;
+  const [characterFav, setCharacterFav] = useState(isFav);
 
   const allEpisodes = useAppSelector((state) => state.RickyReducer.episodes);
 
+  /**
+   * Toma todos los episodios del personaje, por cada uno a su vez hace un nuevo array donde separa
+   * segun los "/" que encuentre. Luego se utiliza el "at()" para tomar el ultimo valor de
+   * cada nuevo array creado con el "split()".
+   * Finalmente se utiliza el array con los numeros de los episodios, segun cada personaje,
+   * para buscar la informacion de los episodios
+   */
   const episodeNumber = characterToUse?.episode?.map((element) =>
     Number(element.split("/").at(-1))
   );
+
   if (episodeNumber && !functionExecuted) {
     episodesDispatch(getEpisodes(episodeNumber));
     setFunctionExecuted(true);
@@ -49,8 +61,11 @@ const PaginaDetalle = () => {
             <p>Planeta: {characterToUse?.origin?.name}</p>
             <p>Genero: {characterToUse?.gender}</p>
           </div>
-          TODO:
-          {/* <BotonFavorito setCharacterFav={} esFavorito={false} /> */}
+          <BotonFavorito
+            esFavorito={characterFav}
+            setCharacterFav={setCharacterFav}
+            idCharacter={characterToUse}
+          />
         </div>
       </div>
       <h4>Lista de episodios donde apareció el personaje</h4>
