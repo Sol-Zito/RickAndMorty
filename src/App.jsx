a183cc787afe139c8d9cfdbbd8424c3cd4aff572
Sol <solzito.1889@gmail.com>
2023-09-12 14:45:33 -0300

    correccion del provider segun readme

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import PaginaInicio from "./paginas/Inicio.pagina";
 import PaginaFavoritos from "./paginas/Favoritos.pagina";
 import PaginaDetalle from "./paginas/Detalle.pagina";
 import Encabezado from "./componentes/layout/encabezado.componente";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <Encabezado />
-      <Routes>
-        <Route path="/" element={<PaginaInicio />} />
-        <Route path="favoritos" element={<PaginaFavoritos />} />
-        <Route path="/detalle/:id" element={<PaginaDetalle />} />
-      </Routes>
+      <Provider store={store}>
+        <Encabezado />
+        <Routes>
+          <Route path="/" element={<PaginaInicio />} />
+          <Route path="favoritos" element={<PaginaFavoritos />} />
+          <Route path="/detalle/:id" element={<PaginaDetalle />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
